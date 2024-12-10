54b64d91787b9de8164260081f015a37db1a7646
byyy <brantyang234@gmail.com>
2024-12-10 14:13:03 -0600

    add frontend and backend code

---
+++ staybooking/src/main/java/com/laioffer/staybooking/location/GeocodingException.java
@@ -0,0 +1,7 @@
+package com.laioffer.staybooking.location;
+
+public class GeocodingException extends RuntimeException {
+  public GeocodingException() {
+    super("Failed to look up address");
+  }
+}
